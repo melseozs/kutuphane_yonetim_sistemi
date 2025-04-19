144e63651a73b8361b77f6cb9df615d5044886e8
ebrarbesir <besirebrar92@gmail.com>
2025-04-19 10:37:43 +0300

    style düzeltmesi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from './Components/Footer';
+import Header from './Components/Header';
 
 function App() {
   return (
--- src/BooksList.jsx
+++ src/BooksList.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import booksData from "./books.json";
+import Header from './Components/Header'; 
 
 const BooksList = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-indigo-100 via-purple-100 to-pink-100 p-6">
+    <>
+    <Header/>
+    <div className="min-h-screen bg-gradient-to-r bg-pink-50 p-6 pt-38">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-10">
         {booksData.map((book) => (
           <div 
             key={book.id}
             className="flex flex-col shadow-lg rounded-xl overflow-hidden bg-white/80 backdrop-blur-sm mx-auto w-60 h-[420px] 
-            border border-pink-200 hover:border-indigo-400 transform hover:scale-105 hover:shadow-indigo-300 transition duration-300"
+            border border-pink-200 hover:border-gray-300 transform hover:scale-105 hover:shadow-gray-500 transition duration-300"
           >
             <div className="h-[65%] w-full">
               <img 
@@ -27,6 +30,7 @@ const BooksList = () => {
         ))}
       </div>
     </div>
+    </>
   );
 };
 
