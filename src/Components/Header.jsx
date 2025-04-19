a8f4f8572537f4aa174cb4e876e16e28529ead40
ebrarbesir <besirebrar92@gmail.com>
2025-04-19 18:21:56 +0300

    hizmetler menüsü düzeltmesi

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.jpeg';
 
-
 function Header({ showSearch = false }) {
   return (
     <header className="bg-[#fffded] w-full fixed top-0 left-0 z-10 p-4 shadow-lg">
       <div className="flex justify-between items-start">
         <div className="text-black text-xl font-bold">
           <div className="flex items-center space-x-3">
-          <img
-            src={logo}
-            alt="Logo"
-            className="h-16 w-16 rounded-lg ml-2.5"
-          />
+            <img
+              src={logo}
+              alt="Logo"
+              className="h-16 w-16 rounded-lg ml-2.5"
+            />
             <span className="text-stone-600 text-xl font-semibold font-sans">
               KÜTÜPHANE YÖNETİM SİSTEMİ
             </span>
@@ -31,9 +30,9 @@ function Header({ showSearch = false }) {
             </Link>
           </div>
           <div className="relative group text-center p-4 hover:bg-white hover:rounded-lg transition duration-300 m-2">
-            <Link to="/hizmetler" className="text-black hover:text-[#35312e] text-xl font-sans">
+            <div className="text-black hover:text-[#35312e] text-xl font-sans cursor-pointer">
               Hizmetler
-            </Link>
+            </div>
             <div className="absolute left-0 hidden group-hover:block bg-white shadow-lg rounded-lg mt-2 w-48 z-20">
               <div className="p-4 hover:bg-orange-50 transition duration-300">
                 <Link to="/login" className="text-black text-lg">Kullanıcı İşlemleri</Link>
@@ -68,20 +67,19 @@ function Header({ showSearch = false }) {
         </nav>
       </div>
       {showSearch && (
-  <div className="w-full bg-[#cfe3e1] py-4 mt-4">
-    <div className="max-w-4xl mx-auto flex items-center space-x-2">
-      <input
-        type="text"
-        placeholder="Arama yap..."
-        className="w-full p-3 text-lg rounded-l-lg border-2 rounded-2xl border-stone-700 bg-white focus:outline-none focus:ring-2 focus:ring-indigo-300 transition duration-300"
-      />
-      <button className="text-black px-4 py-3 rounded-r-lg bg-white hover:bg-gray-100 transition duration-300 border-stone-700 h-13 w-20 shadow-sm hover:shadow-md">
-        Ara
-      </button>
-    </div>
-  </div>
-)}
-
+        <div className="w-full bg-[#cfe3e1] py-4 mt-4">
+          <div className="max-w-4xl mx-auto flex items-center space-x-2">
+            <input
+              type="text"
+              placeholder="Arama yap..."
+              className="w-full p-3 text-lg rounded-l-lg border-2 rounded-2xl border-stone-700 bg-white focus:outline-none focus:ring-2 focus:ring-indigo-300 transition duration-300"
+            />
+            <button className="text-black px-4 py-3 rounded-r-lg bg-white hover:bg-gray-100 transition duration-300 border-stone-700 h-13 w-20 shadow-sm hover:shadow-md">
+              Ara
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
